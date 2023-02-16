885763ea8dd0b45b06bcca4ac00410e4ce388179
HarveyCla <harvey@clarusway.com>
2023-02-16 17:23:37 +0300

    version update

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import "./Header.scss";
+
+const Header = () => {
+    return(
+        <div className="header">
+            <h1 className="text-shadow"> Who Are You ? </h1>
+        </div>
+    )
+}
+
+export default Header;
